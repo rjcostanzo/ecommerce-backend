25343c5f108f0bab9ef1b11e6bdeda30b839e3a8
rjcostanzo <rjcostanzo@gmail.com>
2021-11-14 13:27:10 -0600

    columns for Category, Product, Tag, and ProductTag models

--- models/Category.js
+++ models/Category.js
@@ -1,5 +1,8 @@
+
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
@@ -7,6 +10,18 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
   },
   {
     sequelize,
